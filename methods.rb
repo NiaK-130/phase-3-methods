21a9b5b18d6428ecff7132db398c06439635fc18
Nia K <nsjkappen@gmail.com>
2021-08-30 18:45:22 -0700

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    if name 
+        puts "Hello, #{name}!"
+    elsif name === ""
+        puts "Hello, programmer!"
+    end
+end
+
+
+
+def add(num1, num2)
+    num1 + num2
+  end
+
+
+def halve(num)
+    return nil unless num.class == Integer
+
+    num/2
+    
+  end
